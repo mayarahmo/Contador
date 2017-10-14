d5e7779457492b0a018a9c5283bcbbf476ceb132
Mayara Morais <mayara.morais.santos@gmail.com>
2017-10-14 11:26:54 -0300

    Primeiros pitacos no layout

--- index.php
+++ index.php
@@ -7,61 +7,98 @@
  *
  */
 ?>
-<html>
+<html lang="pt-br">
 <head>
     <title>Contador de palavras</title>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <meta name="description" content="The HTML5 Herald">
     <meta name="author" content="SitePoint">
 
     <!-- BOOSTRAP -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- BOATSTRAP REQUIREMENTS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-
-
 
     <!-- MAIN STYLESHEET -->
     <link rel="stylesheet" href="includes/style/main.css"/>
 </head>
 <body>
-<h1>Contador de palavras</h1>
-<div>
-    <span>Palavras: </span><span class="word-count">0</span><BR/>
-    <span>Caracteres: </span><span class="character-count">0</span><BR/>
-<form name="form-text" method="post" action="">
-    <textarea id="text" name="text" cols="75" rows="15"></textarea><BR/>
-<!--    <button type="submit">Contar</button>-->
-</form>
+<nav class="navbar navbar-expand-md">
+    <a class="navbar-brand" href="#">Contador de palavras</a>
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-
-</div>
-<div>
-    <table>
-        <tr>
-            <th>Estatísticas básicas</th>
-        </tr>
-        <tr>
-            <td>Palavras</td><td><span class="word-count">0</span></td>
-        </tr>
-        <tr>
-            <td>Caracteres</td><td><span class="character-count">0</span></td>
-        </tr>
-        <tr>
-            <th>Outras estatísticas</th>
-        </tr>
-        <tr>
-            <td>Parágrafos</td><td><span class="paragraph-count">0</span></td>
-        </tr>
-        <tr>
-            <td>Sílabas</td><td><span class="">0</span></td>
-        </tr>
+    <div class="navbar-collapse collapse" id="navbarsExampleDefault" style="">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+<div id="main-content" class="col-xs-12">
+    <div id="info-bar" class="col-xs-12">
+        <span>Palavras: </span><span class="word-count">0</span>
+        <span>Caracteres: </span><span class="character-count">0</span><BR/>
+    </div>
+    <div id="main-form"  class="col-xs-12 col-md-2 col-lg-2">
+        <form name="form-text" method="post" action="">
+            <textarea id="text" name="text" cols="75" rows="15"></textarea><BR/>
+            <!--    <button type="submit">Contar</button>-->
+        </form>
+    </div>
+    <div id="main-info-table" class="col-xs-12 col-md-2 col-lg-2">
+        <table id="table-stats">
+            <tr>
+                <th colspan="2">Estatísticas básicas</th>
+            </tr>
+            <tr>
+                <td>Palavras</td><td><span class="word-count">0</span></td>
+            </tr>
+            <tr>
+                <td>Caracteres</td><td><span class="character-count">0</span></td>
+            </tr>
+            <tr>
+                <th colspan="2">Outras estatísticas</th>
+            </tr>
+            <tr>
+                <td>Parágrafos</td><td><span class="paragraph-count">0</span></td>
+            </tr>
+            <tr>
+                <td>Sílabas</td><td><span class="">-</span></td>
+            </tr>
             <td>Sentenças</td><td><span class="sentence-count">1</span></td>
-        <tr>
-            <td>Palavras únicas</td><td><span class="word-count">0</span></td>
-        </tr>
-    </table>
+            <tr>
+                <td>Palavras únicas</td><td><span class="word-count">0</span></td>
+            </tr>
+        </table>
+    </div>
 </div>
 <!-- OS SCRIPTS -->
 <script type="application/javascript">
@@ -128,14 +165,5 @@
     }
 
 </script>
-
-<!-- JQUERY PARA O BOOSTRAP JAVASCRIPT FUNCIONAR -->
-<script
-    src="https://code.jquery.com/jquery-3.2.1.min.js"
-    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-    crossorigin="anonymous"></script>
-<!-- JAVASCRIPT DO BOOTSTRAp -->
-<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/includes/jquery/slideshow/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>-->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
