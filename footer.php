100f7181069fe65dc6587dc66216222f11ae4a43
Mayara Morais <mayara.morais.santos@gmail.com>
2017-12-30 05:49:25 -0200

    Novo estilo

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer class="main-footer">
+    <div class="language-switcher float-left"><a href="#" class="lang-link lang-br" onclick=""><img src="includes/media/bra.png"/></a><a href="#" class="lang-link lang-en" onclick=""><img src="includes/media/uk.png"/></a></div>
     <p class="copyright">Contador de palavras 2018 © Todos os direitos reservados. </p>
 </footer>
 
--- includes/script/jmain.js
+++ includes/script/jmain.js
@@ -4,6 +4,10 @@ $(document).ready( function () {
         $('footer').addClass('post-footer');
     }
 
+    $('lang-link').on('click', function () {
+        $(this).addClass('active');
+    });
+
     $("#word-ocurrences").on('DOMSubtreeModified', function (){
         n = new Number($(this).html());
         v ((n = +n+1) < 10 ? "0" : "") + n;
