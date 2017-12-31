9a6bf08a3c61b9895b0a17e20f6c9181ff0c0132
Mayara Morais <mayara.morais.santos@gmail.com>
2017-12-31 06:18:49 -0200

    Estilo verde

--- about.php
+++ about.php
@@ -7,13 +7,13 @@
 <div class="main-section post-section post-about">
     <div class="col-md-9 post-area">
         <h2>O Contador de palavras</h2>
-        <p class="flow-text">O contador de palavras nasceu em janeiro de 2018 com objetivo de oferecer ferramentas de texto grátis e pretendemos permanecer assim.</p>
+        <p class="flow-text">O contador de palavras nasceu em janeiro de 2018 com objetivo de oferecer ferramentas de texto grátis.</p>
         <p class="flow-text">Atualmente a ferramenta conta com as seguintes funcionalidades:</p>
 
         <ul id="tools-list">
             <li>Inversão de Texto</li>
             <li>Contagem de ocorrência de palavras </li>
-            <li>Contagem de caracteres, sentenças e parágrafos </li>
+            <li>Contagem de caracteres, palavras, sentenças e parágrafos e outras estatísticas</li>
         </ul>
 
         <img class="conceptual-img" src="includes/media/sprouts.png"/>
