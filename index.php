9eccd05b6f08bbcdf239c8c4cf433d41dfcb2032
Mayara Morais <mayara.morais.santos@gmail.com>
2018-01-19 00:32:45 -0200

    Preparaçao para

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
                 <input type="text" name="word" id="word" class="ipt-count-word">&nbsp;<button class="btn-count-word" onclick="countOcurrences(document.getElementById('word').value)">Conta palavra</button><span id="word-ocurrences">00</span>
                 <button onclick="revert()" name="" class="btn-invert-text">Inverte texto &#8645;</button><br/>
-                <input type="text" name="lorem" id="lorem" class="ipt-count-lorem" placeholder="Qtd de parágrafos">&nbsp;<button class="btn-lorem" onclick="countOcurrences(document.getElementById('word').value)">Gerador de lorem ipsum</button>
+<!--                <input type="text" name="lorem" id="lorem" class="ipt-count-lorem" placeholder="Qtd de parágrafos">&nbsp;<button class="btn-lorem" onclick="countOcurrences(document.getElementById('word').value)">Gerador de lorem ipsum</button>-->
 <!--
                 </form>-->
         </div>
@@ -49,10 +49,10 @@
 <!--            <tr>-->
 <!--                <td>Sílabas</td><td><span class="">-</span></td>-->
 <!--            </tr>-->
-            <td>Sentenças</td><td><span class="sentence-count">01</span></td>
-            <tr>
-                <td>Palavras únicas</td><td><span class="word-count">00</span></td>
-            </tr>
+                <td>Sentenças</td><td><span class="sentence-count">01</span></td>
+<!--            <tr>-->
+<!--                <td>Palavras únicas</td><td><span class="word-count">00</span></td>-->
+<!--            </tr>-->
             <tr>
                 <th colspan="2">Densidade de palavras (Em breve)</th>
             </tr>
